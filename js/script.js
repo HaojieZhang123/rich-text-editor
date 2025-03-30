e914d3b2a01beb45751cb71bfc42509f65fb6729
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-30 01:42:18 +0100

    [UPD] small fixes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Import all buttons and elements
 let optionsButtons = document.querySelectorAll(".option-button");
 let advancedOptionButton = document.querySelectorAll(".adv-option-button");
-let fontName = document.getElementById("font");
+let fontName = document.getElementById("fontName");
 let fontSizeRef = document.getElementById("fontSize");
 let writingArea = document.getElementById("text-input");
 let alignButtons = document.querySelectorAll(".align");
